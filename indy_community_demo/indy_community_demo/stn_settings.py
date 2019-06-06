bbc045cb5ad5da304a0c8ce8aa859009a2f09759
Ian Costanzo <ian@anon-solutions.ca>
2019-06-06 06:50:39 -0700

    Added configs for test agencies
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- indy_community_demo/indy_community_demo/stn_settings.py
+++ indy_community_demo/indy_community_demo/stn_settings.py
@@ -11,9 +11,19 @@ INDY_CONFIG['storage_config'] = {'url': 'wallet-db:5432'}
 # from VCX [SERVER_ENVIRONMENT.STAGING] config (connector-app/app/store/config-store.js)
 #INDY_CONFIG['vcx_agency_url'] = 'http://dummy-cloud-agent:8080'
 INDY_CONFIG['vcx_agency_url'] = 'http://vcx-agency.anon-solutions.ca:8080'
+
 #INDY_CONFIG['vcx_agency_url'] = 'https://agency.pstg.evernym.com'
 #INDY_CONFIG['vcx_agency_did'] = 'LqnB96M6wBALqRZsrTTwda'
 #INDY_CONFIG['vcx_agency_verkey'] = 'BpDPZHLbJFu67sWujecoreojiWZbi2dgf4xnYemUzFvB'
+
+#INDY_CONFIG['vcx_agency_url'] = 'https://eas01.pps.evernym.com'
+#INDY_CONFIG['vcx_agency_did'] = 'UNM2cmvMVoWpk6r3pG5FAq'
+#INDY_CONFIG['vcx_agency_verkey'] = 'FvA7e4DuD2f9kYHq6B3n7hE7NQvmpgeFRrox3ELKv9vX'
+
+#INDY_CONFIG['vcx_agency_url'] = 'https://agency.pps.evernym.com'
+#INDY_CONFIG['vcx_agency_did'] = '3mbwr7i85JNSL3LoNQecaW'
+#INDY_CONFIG['vcx_agency_verkey'] = '2WXxo6y1FJvXWgZnoYUP5BJej2mceFrqBDNPE3p6HDPf'
+
 INDY_CONFIG['vcx_genesis_path'] = '/tmp/stn-genesis.txt'
 # TBD not sure if there is an STN browser available
 INDY_CONFIG['ledger_url']      = 'http://sovrin-stn-browser.vonx.io'
